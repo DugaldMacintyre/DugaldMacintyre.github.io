c5e303b9f0ceed119c8fc63b022f487fc5c2058e
Dugald Macintyre <114068745+DugaldMacintyre@users.noreply.github.com>
2023-09-03 17:52:24 +0100

    test: update when deployed

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <p>Hi, my name is Dugald</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
