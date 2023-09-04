48c927752c2e0538e3fc256e0a2e79cc60de2fb2
Dugald Macintyre <114068745+DugaldMacintyre@users.noreply.github.com>
2023-09-04 18:31:01 +0100

    add: about, skills, and qualifications sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
-
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Qualification from './components/qualification/Qualification';
+import ScrollUp from './components/scrollup/ScrollUp';
 
 function App() {
 
   return (
     <>
       <Navbar />
+
+      <main className="main">
+        <Home />
+        <About />
+        <Skills />
+        <Qualification />
+      </main>
+
+      <ScrollUp />
     </>
   )
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/dugald-macintyre-29a537261/" className="home__social-icon" target="_blank">
+          <i class='bx bxl-linkedin' ></i>
+        </a>
+
+        <a href="https://github.com/DugaldMacintyre" className="home__social-icon" target="_blank">
+          <i className='uil uil-github-alt'></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
